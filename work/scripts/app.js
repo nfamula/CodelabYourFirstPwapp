d5886f665a0e2048c568a2542831f82f390d0259
n-famula <n.famula@giantsandgents.com>
2017-03-31 14:42:36 -0400

    service workers to pre-cache App Shell

--- work/scripts/app.js
+++ work/scripts/app.js
@@ -343,4 +343,9 @@
   }
 
   // TODO add service worker code here
+    if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
 })();
